f598fc86fa4d23dde313f556e14f31c941b187e8
Mohammad Sumon <sumon3011@gmail.com>
2022-10-26 11:32:12 +0600

    some data added

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./darta/categories.json');
+const categories = require('./data/categories.json');
+const categoryDetails = require('./data/categoryDetails.json');
 
 app.get('/', (req, res) => {
     res.send('Programming data is running')
@@ -13,6 +14,12 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = categoryDetails.find(n => n.id === id)
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
